0e79bad8e80abbb85d6fa7b334309d4e0c80b225
Administrator <Administrator@TRVC-PC26>
2024-06-04 16:23:43 +0700

    ok

--- TRVC/Program.cs
+++ TRVC/Program.cs
@@ -16,7 +16,7 @@ namespace TRVC
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new CashVND());
+            Application.Run(new Code());
         }
     }
 }
